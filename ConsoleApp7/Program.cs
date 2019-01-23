842ec3b0fe5494f7cfb1a9fd7a1c2013e5ef3078
saksham rana <0blackbuck@gmail.com>
2019-01-22 21:32:18 -0500

    new

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -12,71 +12,72 @@ namespace ConsoleApp7
                 int marks;
                 marks = Convert.ToInt32(Console.ReadLine());
 
-                int get_your_grade = cal_grade(marks);
+                string get_your_grade = cal_grade(marks);
 
             }
             catch { }
         }//end of main
-        private static int cal_grade(int get_marks)
+        private static string cal_grade(int get_marks)
         {
+            string a = " ";
             if (get_marks >= 95)
             {
-                string a = "A+";
+                a = "A+";
                 Console.WriteLine("Your grade is :"  + a);
             }
             else if (get_marks >= 90)
             {
-                string a = "A";
+                a = "A";
                 Console.WriteLine("Your grade is :" + a);
             }
             else if (get_marks >= 87)
             {
-                string a = "A-";
+                a = "A-";
                 Console.WriteLine("Your grade is :" + a);
             }
             else if (get_marks >= 84)
             {
-                string a = "B+";
+                a = "B+";
                 Console.WriteLine("Your grade is :" + a);
             }
             else if (get_marks >= 80)
             {
-                string a = "B";
+                a = "B";
                 Console.WriteLine("Your grade is B" + a);
             }
             else if (get_marks >= 77)
             {
-                string a = "B-";
+                a = "B-";
                 Console.WriteLine("Your grade is :" + a);
             }
             else if (get_marks >= 74)
             {
-                string a = "C+";
+                a = "C+";
                 Console.WriteLine("Your grade is :" + a);
             }
             else if (get_marks >= 70)
             {
-                string a = "C";
+                a = "C";
                 Console.WriteLine("Your grade is C:" + a);
             }
             else if (get_marks >= 67)
             {
-                string a = "C-";
+                a = "C-";
                 Console.WriteLine("Your grade is :" + a);
             }
             else if (get_marks >= 63)
             {
-                string a = "D+";
+                a = "D+";
                 Console.WriteLine("Your grade is :" + a);
             }
             else if (get_marks >= 60)
             {
-                string a = "D";
+                a = "D";
                 Console.WriteLine("Your grade is D" + a);
             }
             else
             {
-                string a = "F";
+                a = "F";
                 Console.WriteLine("Your grade is :" + a);
             }
             return a;
